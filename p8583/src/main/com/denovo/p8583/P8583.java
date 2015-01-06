f2d22480498bddd5f83e71949c5dc62bb5405e02
moon.wa <moon.ww.z@gmail.com>
2015-01-06 22:34:12 +0800

    init

---
+++ p8583/src/main/com/denovo/p8583/P8583.java
@@ -0,0 +1,6 @@
+package com.denovo.p8583;
+public class P8583{
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ScheduledTasks.class);
+    }
+}
---
+++ p8583server/src/main/com/denovo/p8583server/App.java
@@ -0,0 +1,6 @@
+package com.denovo.p8583server;
+public class App{
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ScheduledTasks.class);
+    }
+}
